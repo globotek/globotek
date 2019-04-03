d025d9ff205afd4b8cb2e0b5da8a1c9fee07270f
alexdelmont <alex.delmont@gmail.com>
2019-04-03 11:46:45 +0100

    Contact form is now a stanalone component

--- wp-content/themes/globotek/partials/contact-form.php
+++ wp-content/themes/globotek/partials/contact-form.php
@@ -7,52 +7,52 @@
  */
 ?>
 
-<div class="cta">
+<div class="contact-form">
 	
-	<div class="cta__background">
+	<div class="contact-form__background">
 		<img src="<?php echo get_template_directory_uri() . '/images/contact-bg.svg'; ?>"/>
 	</div>
 	
-	<div class="cta__inner">
+	<div class="contact-form__inner">
 		
-		<div class="cta__contact form">
+        <div class="contact-form__form form">
 			
-			<div class="cta__contact__content">
-				<div class="cta__contact__content__title title title__secondary">Lorem ipsum dolor sit amet</div>
-				<div class="cta__contact__content__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et vestibulum arcu. Aenean quis orci sem. Suspendisse iaculis scelerisque purus ornare finibus. Donec maximus mauris vel interdum pharetra.</div>
+			<div class="contact-form__content">
+				<div class="contact-form__contact__content__title title title__secondary">Lorem ipsum dolor sit amet</div>
+				<div class="contact-form__contact__content__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et vestibulum arcu. Aenean quis orci sem. Suspendisse iaculis scelerisque purus ornare finibus. Donec maximus mauris vel interdum pharetra.</div>
             </div>
 
-            <div class="cta__contact__prompt">
-                <div class="cta__contact__prompt-inner">
+            <div class="contact-form__prompt">
+                <div class="contact-form__prompt--inner">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 </div>
             </div>
             
-            <div class="cta__contact__left">
-                <div class="cta__contact__field">
+            <div class="contact-form__left">
+                <div class="contact-form__contact__field">
                     <input type="text" class="form__field" placeholder="Name" />
                 </div>
                 
-                <div class="cta__contact__field">
+                <div class="contact-form__field">
                     <input type="text" class="form__field" placeholder="Email" />
                 </div>
 
-                <div class="form__range" data-module="range-slider">
-                    <div class="cta__contact__content">
+                <div class="contact-form__range form__range" data-module="range-slider">
+                    <div class="contact-form__content">
                         Budget
                     </div>
-                    <input class="form__range-input js-range__trigger" type="range" min="1000" max="25000" step="100" value="200" data-thumbwidth="20">
-                    <output class="form__range-output" name="range-value">£100</output>
+                    <input class="form__range--input js-range__trigger" type="range" min="1000" max="25000" step="100" value="200" data-thumbwidth="20">
+                    <output class="form__range--output" name="range-value">£100</output>
                 </div>
 			</div>
 			
-			<div class="cta__contact__right">
-                <textarea class="form__textarea" placeholder="Message"></textarea>
+			<div class="contact-form__right">
+                <textarea class="contact-form__textarea form__textarea" placeholder="Message"></textarea>
 
 				<a href="#" class="button button--white">Send</a>
 			</div>
 		
-		</div>
+        </div>
 	
 	</div>
 
