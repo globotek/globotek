451ad6daa0c0658f2eaec61b6329175aa1cbf3f4
alexdelmont <alex.delmont@gmail.com>
2019-04-04 14:45:33 +0100

    Single.php Refactor Amend

--- wp-content/themes/globotek/home.php
+++ wp-content/themes/globotek/home.php
@@ -35,48 +35,51 @@
 				
 				<a href="<?php the_permalink(); ?>" class="blog__image">
 					<?php the_post_thumbnail( 'interior-banner' ); ?>
-				</a>
-				
-				<div class="blog__body blog__card__body">
-					
-					<div class="blog__heading">
-						
-						<div class="blog__heading__date">
-							<div class="post-date">
-								<span><?php the_time( 'M' ); ?></span><?php the_time( 'd' ); ?>
-							</div>
-						</div>
-						
-						<a href="<?php the_permalink(); ?>" class="blog__heading__title">
-							<h1 class="title title__secondary"><?php the_title(); ?></h1>
-						</a>
-					
-					</div>
-					
-					
-					<div class="blog__body__meta">
-                        
-                    <div class="tag-list">
-                        <p>
-                            <span class="tag-list__author"><?php the_author(); ?></span>
-                            <?php foreach ( $categories as $category ) {
-                                echo ' | <a href="' . get_term_link( $category ) . '">' . $category->name . '</a>';
-                            } ?>
-                        </p>
-                    </div>
+                </a>
 
-					</div>
-					
-					<div class="blog__body__text">
-						<p><?php the_excerpt(); ?></p>
-					</div>
-					
-					<div class="blog__body__link">
-						<a href="<?php the_permalink(); ?>" class="button__text button__text--blue">Read More
-							<i class="fas fa-arrow-right"></i></a>
-					</div>
-				
-				</div>
+                <div class="blog__card__inner">
+                
+                    <div class="blog__heading">
+                            
+                        <div class="blog__heading__date">
+                            <div class="post-date">
+                                <span><?php the_time( 'M' ); ?></span><?php the_time( 'd' ); ?>
+                            </div>
+                        </div>
+                        
+                        <a href="<?php the_permalink(); ?>" class="blog__heading__title">
+                            <h1 class="title title__secondary"><?php the_title(); ?></h1>
+                        </a>
+                    
+                    </div>
+                    
+                    <div class="blog__body">					
+                        
+                        <div class="blog__body__meta">
+                            
+                        <div class="tag-list">
+                            <p>
+                                <span class="tag-list__author"><?php the_author(); ?></span>
+                                <?php foreach ( $categories as $category ) {
+                                    echo ' | <a href="' . get_term_link( $category ) . '">' . $category->name . '</a>';
+                                } ?>
+                            </p>
+                        </div>
+
+                        </div>
+                        
+                        <div class="blog__body__text">
+                            <p><?php the_excerpt(); ?></p>
+                        </div>
+                        
+                        <div class="blog__body__link">
+                            <a href="<?php the_permalink(); ?>" class="button__text button__text--blue">Read More
+                                <i class="fas fa-arrow-right"></i></a>
+                        </div>
+                    
+                    </div>
+                
+                </div>
 			
 			</div>
 		
