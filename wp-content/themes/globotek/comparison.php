8613013b02e57ae45f78e8f814a02a7402b96a7c
alexdelmont <alex.delmont@gmail.com>
2019-04-11 13:00:15 +0100

    Added Timeline Page Template

---
+++ wp-content/themes/globotek/comparison.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Comparison
+ * Created by PhpStorm.
+ * User: wmjc1
+ * Date: 18/02/2019
+ * Time: 19:43
+ */ ?>
+
+<?php get_header(); ?>
+
+<?php gtek_hero(); ?>
+
+<div class="comparison-page wrapper">
+	
+	
+
+</div>
+
+<?php get_footer(); ?>
