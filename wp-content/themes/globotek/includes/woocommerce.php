57b2913bc092205152f1a5430c3318b2cddc375a
alexdelmont <alex.delmont@gmail.com>
2019-05-28 12:29:05 +0100

    Single product page css

--- wp-content/themes/globotek/includes/woocommerce.php
+++ wp-content/themes/globotek/includes/woocommerce.php
@@ -18,6 +18,9 @@ remove_action('woocommerce_product_thumbnails', 'woocommerce_show_product_thumbn
 remove_action('woocommerce_before_single_product_summary', 'woocommerce_show_product_sales_flash', 10);
 remove_action('woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20);
 
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
+
+
 remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
 remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
 remove_action('woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15);
@@ -42,7 +45,7 @@ add_action('woocommerce_before_single_product', 'woocommerce_template_single_tit
 add_action('woocommerce_before_single_product', 'woocommerce_show_product_images', 20);
 add_action('woocommerce_before_single_product', 'woocommerce_template_single_excerpt', 20);
 
-add_action('woocommerce_after_single_product_summary', 'woocommerce_template_single_price', 10);
-add_action('woocommerce_after_single_product_summary', 'woocommerce_template_single_add_to_cart', 20);
+add_action('woocommerce_before_single_product_summary', 'woocommerce_template_single_price', 10);
+add_action('woocommerce_before_single_product_summary', 'woocommerce_template_single_add_to_cart', 20);
 
 add_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 30);
--- wp-content/themes/globotek/woocommerce/content-single-product.php
+++ wp-content/themes/globotek/woocommerce/content-single-product.php
@@ -16,61 +16,103 @@
  */
 
 defined( 'ABSPATH' ) || exit;
-
-/**
- * Hook: woocommerce_before_single_product.
- *
- * @hooked wc_print_notices - 10
- */
-do_action( 'woocommerce_before_single_product' );
-
-if ( post_password_required() ) {
-	echo get_the_password_form(); // WPCS: XSS ok.
-	return;
-}
 ?>
-<div id="product-<?php the_ID(); ?>" <?php wc_product_class(); ?>>
 
-	<?php
-		/**
-		 * Hook: woocommerce_before_single_product_summary.
-		 *
-		 * @hooked woocommerce_show_product_sale_flash - 10
-		 * @hooked woocommerce_show_product_images - 20
-		 */
-		do_action( 'woocommerce_before_single_product_summary' );
-	?>
+<div class="woocommerce-product">
 
-	<div class="summary entry-summary">
-		<?php
-			/**
-			 * Hook: woocommerce_single_product_summary.
-			 *
-			 * @hooked woocommerce_template_single_title - 5
-			 * @hooked woocommerce_template_single_rating - 10
-			 * @hooked woocommerce_template_single_price - 10
-			 * @hooked woocommerce_template_single_excerpt - 20
-			 * @hooked woocommerce_template_single_add_to_cart - 30
-			 * @hooked woocommerce_template_single_meta - 40
-			 * @hooked woocommerce_template_single_sharing - 50
-			 * @hooked WC_Structured_Data::generate_product_data() - 60
-			 */
-			do_action( 'woocommerce_single_product_summary' );
-		?>
-	</div>
+    <div class="woocommerce-product__left">
 
-	<?php
-		/**
-		 * Hook: woocommerce_after_single_product_summary.
-		 *
-		 * @hooked woocommerce_output_product_data_tabs - 10
-		 * @hooked woocommerce_upsell_display - 15
-		 * @hooked woocommerce_output_related_products - 20
-		 */
-		do_action( 'woocommerce_after_single_product_summary' );
-	?>
+        <?php 
+        /**
+         * Hook: woocommerce_before_single_product.
+         *
+         * @hooked wc_print_notices - 10
+         */
+        do_action( 'woocommerce_before_single_product' );
 
-    ekjgerjkvnerkjnvekr
+        if ( post_password_required() ) {
+            echo get_the_password_form(); // WPCS: XSS ok.
+            return;
+        }
+        ?>
+
+    </div>
+
+    <div class="woocommerce-product__right">
+
+        <div id="product-<?php the_ID(); ?>" <?php wc_product_class(); ?>>
+
+            <div class="gradient-box">
+
+                <?php
+                    /**
+                     * Hook: woocommerce_before_single_product_summary.
+                     *
+                     * @hooked woocommerce_show_product_sale_flash - 10
+                     * @hooked woocommerce_show_product_images - 20
+                     */
+                    do_action( 'woocommerce_before_single_product_summary' );
+                ?>
+
+                
+        
+                <div class="summary entry-summary">
+                
+                    <?php
+                        /**
+                         * Hook: woocommerce_single_product_summary.
+                         *
+                         * @hooked woocommerce_template_single_title - 5
+                         * @hooked woocommerce_template_single_rating - 10
+                         * @hooked woocommerce_template_single_price - 10
+                         * @hooked woocommerce_template_single_excerpt - 20
+                         * @hooked woocommerce_template_single_add_to_cart - 30
+                         * @hooked woocommerce_template_single_meta - 40
+                         * @hooked woocommerce_template_single_sharing - 50
+                         * @hooked WC_Structured_Data::generate_product_data() - 60
+                         */
+                        do_action( 'woocommerce_single_product_summary' );
+                    ?>
+                </div>
+
+            </div>
+
+            <div class="support">
+
+                <h3>Support</h3>
+
+                <p>Fusce massa odio, fringilla eget nisi elementum, rhoncus consectetur risus.</p>
+
+                <div class="center">
+                    <a class="button--white">
+                        Ask Us a Question
+                    </a>
+                </div>
+
+            </div>
+
+            <div class="details">
+
+                <?php
+                    /**
+                     * Hook: woocommerce_after_single_product_summary.
+                     *
+                     * @hooked woocommerce_output_product_data_tabs - 10
+                     * @hooked woocommerce_upsell_display - 15
+                     * @hooked woocommerce_output_related_products - 20
+                     */
+                    do_action( 'woocommerce_after_single_product_summary' );
+                ?>
+
+                <p><a href="">Changelog</a></p>
+
+            </div>
+
+            
+        </div>
+        
+        <?php do_action( 'woocommerce_after_single_product' ); ?>
+
+    </div>
+
 </div>
-
-<?php do_action( 'woocommerce_after_single_product' ); ?>
