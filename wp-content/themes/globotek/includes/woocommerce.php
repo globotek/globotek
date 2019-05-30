5f4f080ce253ed3fce749fc79a9718351ddc240b
alexdelmont <alex.delmont@gmail.com>
2019-05-30 10:57:48 +0100

    Removed hooks from single product page

--- wp-content/themes/globotek/includes/woocommerce.php
+++ wp-content/themes/globotek/includes/woocommerce.php
@@ -44,6 +44,13 @@ remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_p
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 );
 remove_action( 'woocommerce_after_single_product_summary', '', 20 );
 
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
+
 function clear_add_to_cart() {
 	
 	if ( isset( $_GET[ 'add-to-cart' ] ) ) {
