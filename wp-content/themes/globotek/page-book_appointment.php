de82a9aa4e9b002169513cad902f08f0713ea4c1
Matt Campbell <matthew@globotek.net>
2019-06-10 18:28:24 +0100

    Refactor datepicker filename to current

--- wp-content/themes/globotek/page-book_appointment.php
+++ wp-content/themes/globotek/page-book_appointment.php
@@ -10,45 +10,29 @@
 get_header(); ?>
 
 <div class="chunk">
-
+	
 	
 	<div class="grid grid--halves wrapper" data-module="freshsales">
 		
 		<div class="grid__item">
 			
 			<p>Content</p>
-			
+		
 		</div>
 		
 		<div class="grid__item">
 			
-			<div class="js-datepicker"></div>
-			<input type="text" class="js-datepicker-selection"/>
+			<div class="datepicker">
+				
+				<div class="js-datepicker"></div>
+				<input type="text" class="js-datepicker-selection"/>
+			
+			</div>
 		
 		</div>
 	
 	</div>
-<<<<<<< HEAD
-=======
-=======
-
-    <div class="wrapper">
-
-        <div class="book-appointment" data-module="freshsales">
-
-            <div class="datepicker">
-            
-                <div class="js-datepicker"></div>
-                <input type="text" class="js-datepicker-selection"/>
-
-            </div>
-
-        </div>
-
-    </div>
->>>>>>> aec7a8a0cbd8349c592e3acb76f79dc1ba3010e5
->>>>>>> 09a56fe55cffb8eaca4e5fccc97021cbbfe28d94
-
+	
 </div>
 
 <?php get_footer(); ?>
