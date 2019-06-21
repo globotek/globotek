32a5e500213e2a41610129c58e7dc5ad9bbeda89
alexdelmont <alex.delmont@gmail.com>
2019-06-21 09:56:41 +0100

    Fixed button opacity bug on appointment page

--- framework/themes/globotek/scripts/modules/_form-page.js
+++ framework/themes/globotek/scripts/modules/_form-page.js
@@ -78,9 +78,9 @@
 
             function hideButtons() {
                 if ($('.form-slider__page:first-child').hasClass('active')) {
-                    $('.back').css('opacity', '0');
+                    $('.back').hide();
                 } else {
-                    $('.back').css('opacity', '1');
+                    $('.back').show();
                 }
 
                 if ($('.form-slider__page:nth-last-child(2)').hasClass('active')) {
